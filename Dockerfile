25a323f1820465c6f009bb35755300e4b0d697ba
oscarvo29 <oscar.vinther@gmail.com>
2023-05-31 09:51:42 +0200

    Fixed teh dockerfile, and added some default properties

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
 FROM openjdk:latest
 COPY target/adonis-0.0.1-SNAPSHOT.jar /usr/src/snap.jar
-CMD java -jar /user/src/snap.jar
+CMD java -jar /usr/src/snap.jar
--- src/main/java/com/example/adonis/AdonisApplication.java
+++ src/main/java/com/example/adonis/AdonisApplication.java
@@ -11,6 +11,13 @@ public class AdonisApplication {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(AdonisApplication.class);
         app.setDefaultProperties(Collections.singletonMap("server.port", "5050"));
+        app.setDefaultProperties(Collections.singletonMap("spring.datasource.url", "jdbc:mysql://192.168.0.11/adonis"));
+        app.setDefaultProperties(Collections.singletonMap("spring.datasource.username", "4918Demk!"));
+        app.setDefaultProperties(Collections.singletonMap("spring.jpa.generate-ddl", "true"));
+        app.setDefaultProperties(Collections.singletonMap("spring.jpa.hibernate.ddl-auto", "create-drop "));
+        app.setDefaultProperties(Collections.singletonMap("spring.sql.init.mode", "always"));
         app.run(args);
     }
 }
+
+
